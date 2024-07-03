48ce5657f8e7fdf267d6fbd6e682ba1bb9e86c67
Dageron30 <mdrake2023@fau.edu>
2024-07-03 18:52:28 -0400

    Update barrier.cc

--- barrier.cc
+++ barrier.cc
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <pthread.h>
-#include <unistd.h> // for sleep
+#include <unistd.h>
 #include <vector>
-
+// barrier class declaration
 class Barrier {
-public:
+public:  // only constructor, destructor, and wait function are public
     Barrier(int n);
     ~Barrier();
     void wait();
@@ -15,58 +15,56 @@ private:
     int count;
     int num_threads;
 };
-
+// constructor for barrier using n as number of threads
 Barrier::Barrier(int n) : num_threads(n), count(0) {
     pthread_mutex_init(&mutex, nullptr);
     pthread_cond_init(&cond, nullptr);
 }
 
-Barrier::~Barrier() {
+Barrier::~Barrier() { // destructor destroys mutex and cond variable
     pthread_mutex_destroy(&mutex);
     pthread_cond_destroy(&cond);
 }
-
-void Barrier::wait() {
-    pthread_mutex_lock(&mutex);
-    count++;
-    if (count < num_threads) {
+// causes all threads to wait till the count reaches total threads
+void Barrier::wait() {  
+    pthread_mutex_lock(&mutex); //locks
+    count++;                    //increments count
+    if (count < num_threads) {  //causes thread to wait till we reach the last thread
+    	std::cout << "Thread " << count << " is waiting!" << std::endl;
         pthread_cond_wait(&cond, &mutex);
     } else {
-        count = 0; // Reset count for reuse
-        pthread_cond_broadcast(&cond);
+        sleep(1); //delay for work
+        std::cout << "Thread " << count << " released the other threads!" << std::endl;
+        count = 0; // seset count for reuse
+        pthread_cond_broadcast(&cond);  //starts all threads back up
     }
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutex); // unlocks
 }
 
-// Thread function
+// thread function
 void* thread_fun(void* param) {
     Barrier* barrier = static_cast<Barrier*>(param);
-    while (true) {
-        // Do some work
+    while (true) {  //infinite loop of waiting and all releasing at same time
         std::cout << "Thread " << pthread_self() << " is doing work before the barrier." << std::endl;
-        sleep(rand() % 3); // Simulate work with sleep
-
-        // Wait at the barrier
-        barrier->wait();
-
-        // Do some more work
-        std::cout << "Thread " << pthread_self() << " is doing work after the barrier." << std::endl;
-        sleep(rand() % 3); // Simulate work with sleep
+        sleep(1); //delay for work
+        barrier->wait();  //each thread waits till broadcast is sent
+        std::cout << "Thread " << pthread_self() << " continued work." << std::endl;
+        sleep(1); //delay for work
     }
     return nullptr;
 }
 
 int main() {
-    const int N = 5; // Number of threads
+    const int N = 5; // using 5 threads total 
     Barrier barrier(N);
 
-    // Create and start N threads
+    // create and start N threads with a thread vector
     std::vector<pthread_t> threads(N);
     for (int i = 0; i < N; ++i) {
         pthread_create(&threads[i], nullptr, thread_fun, &barrier);
     }
 
-    // Join threads (though they run indefinitely in this example)
+    // join threads
     for (int i = 0; i < N; ++i) {
         pthread_join(threads[i], nullptr);
     }
