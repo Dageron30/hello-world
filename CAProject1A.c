06821a7fe1dae6565575b319bb1abf3786d80dcd
Dageron30 <mdrake2023@fau.edu>
2024-07-08 14:24:59 -0400

    Proj1 done

--- CAProject1A.c
+++ CAProject1A.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <string.h>
-int main ()  {
+int main ()
+{
     int age[3], i = 0, j = 0;
     char name[3][50];
     double salary[3];
-    for(i = 0; i < 3; i++) {
-        printf("Enter the name of employee %d: ", i+1);   //prompt the user to enter name
-        scanf("%s", name[i]);                             //scan user’s input
+    for(i = 0; i < 3; i++)
+    {
+        printf("Enter the name of employee %d: ", i+1); //prompt the to enter name
+        scanf("%s", name[i]); //scan user’s input
         printf("Enter the age of employee %d: ", i+1);    //prompt the to enter age
-        scanf("%s", age[i]);                              //scan user’s input
+        scanf("%d", &age[i]);                              //scan user’s input
         printf("Enter the salary of employee %d: ", i+1); //prompt the to enter salary
-        scanf("%s", salary[i]);                           //scan user’s input
-        printf("\n");                                     //a new line
+        scanf("%lf", &salary[i]);                           //scan user’s input
+        printf("\n"); //a new line
     }
-    for(j = 0; j < 3; j++) {
+    for(j = 0; j < 3; j++)
+    {
         //print Prints information (name, age, salary) of each employee in a separate line
         printf("The name of employee %d is: %s. They are %d years old,
         and their salary is $%.2f.\n", j+1, name[j], age[j], salary[j]);
-    }
+        }
     return 0;
 }
