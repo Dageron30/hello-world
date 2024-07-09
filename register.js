494e23c10b550a4dc8eb2192a756db879ea278f7
Dageron30 <mdrake2023@fau.edu>
2024-07-08 20:53:23 -0400

    update

--- register.js
+++ register.js
@@ -1,5 +1,17 @@
 function checkForm() {
-    // TODO: Perform input validation 
+    let areFormErrors = false;
+    let errorMessages = "<ul>";
+    const fullNameElement = document.getElementById("fullName");
+    if (fullNameElement.value.length === 0) {
+        errorMessages += "<li>Missing full name.</li>";
+        fullNameElement.classList.add("error");
+        areFormErrors = true;
+    } else {
+        fullNameElement.classList.remove("error");
+        areFormErrors = false;
+    }
+    // check email syntax
+    // check password meets requirements 
  }
  
  document.getElementById("submit").addEventListener("click", function(event) {
